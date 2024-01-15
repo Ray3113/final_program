f770c39fdf48ea23e32f37b95753ea7fd223a181
Ray3113 <92814756+Ray3113@users.noreply.github.com>
2024-01-15 15:13:06 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'dart:async';
 import 'dart:io';
+class Music {
+  final String title;
+  final String filePath;
+
+  Music({
+    required this.title,
+    required this.filePath,
+  });
+}
+
+Map<String, List<Music>> languageMusicMap = {
+  'Chinese': [
+    Music(title: 'Chinese Song 1', filePath: 'Chinese/3.mp3'),
+
+  ],
+  'Japanese': [
+    Music(title: 'Japanese Song 1', filePath: 'Japanese/4.mp3'),
+    Music(title: 'Japanese Song 2', filePath: 'Japanese/7.mp3'),
+
+  ],
+  'English': [
+    Music(title: 'English Song 1', filePath: 'English/1.mp3'),
+    Music(title: 'English Song 2', filePath: 'English/2.mp3'),
+  ],
+};
 
 void main() {
   runApp(MusicPlayerApp());
@@ -48,13 +73,10 @@ class _SplashScreenState extends State<SplashScreen> {
 
 Future<List<String>> _getMp3Files(String languageFolder) async {
   try {
-    // 指定路径
     List<String> mp3Files = [];
+    String folderPath = languageFolder;
+    Directory directory = Directory(folderPath);
 
-    String file_path = Directory.current.path + languageFolder;
-    Directory directory = Directory(file_path);
-
-    // 查找所有MP3文件
     List<FileSystemEntity> fileList = directory.listSync(recursive: true);
     for (var file in fileList) {
       if (file is File && file.path.endsWith('.mp3')) {
@@ -62,38 +84,15 @@ Future<List<String>> _getMp3Files(String languageFolder) async {
       }
     }
 
-    for (var mp3File in mp3Files) {
-      print('MP3 File: ${mp3File}');
-    }
-
     return mp3Files;
-
   } catch (e) {
     print('Error: $e');
     return Future.error('Failed to get MP3 files');
   }
-
 }
 
-// Future<List<String>> _getMp3Files(String languageFolder) async {
-//   Directory directory = Directory('assets/$languageFolder/'); // 替換成你的音樂檔案所在的目錄
-//   List<String> mp3Files = [];
-//
-//   try {
-//     List<FileSystemEntity> files = directory.listSync();
-//     for (FileSystemEntity file in files) {
-//       if (file is File && file.path.endsWith('.mp3')) {
-//         mp3Files.add(file.path);
-//       }
-//     }
-//   } catch (e) {
-//     print('Error reading mp3 files: $e');
-//   }
-//
-//   return mp3Files;
-// }
 
-void _navigateToMusicPlayer(BuildContext context, List<String> mp3Files) {
+void _navigateToMusicPlayer(BuildContext context, String mp3Files) {
   Navigator.pushReplacement(
     context,
     MaterialPageRoute(builder: (context) => MusicPlayerScreen(mp3Files)),
@@ -113,21 +112,21 @@ class LanguageSelectionScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () async {
-                List<String> mp3Files = await _getMp3Files('English');
+                String mp3Files = "English";
                 _navigateToMusicPlayer(context, mp3Files);
               },
               child: Text('English'),
             ),
             ElevatedButton(
               onPressed: () async {
-                List<String> mp3Files = await _getMp3Files('Chinese');
+                String mp3Files = "Chinese";
                 _navigateToMusicPlayer(context, mp3Files);
               },
               child: Text('中文'),
             ),
             ElevatedButton(
               onPressed: () async {
-                List<String> mp3Files = await _getMp3Files('Japanese');
+                String mp3Files = "Japanese";
                 _navigateToMusicPlayer(context, mp3Files);
               },
               child: Text('日本語'),
@@ -141,115 +140,12 @@ class LanguageSelectionScreen extends StatelessWidget {
 
 
 class MusicPlayerScreen extends StatefulWidget {
-  final List<String> mp3Files;
+  final String mp3Files;
   MusicPlayerScreen(this.mp3Files);
   @override
   _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
 }
 
-// class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
-//   AudioPlayer _audioPlayer = AudioPlayer();
-//   bool isPlaying = false;
-//   int currentIndex = 0;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _loadMusic();
-//   }
-//
-//   void _loadMusic() async {
-//     await _audioPlayer.play(AssetSource(musicList[currentIndex].filePath));
-//   }
-//
-//   void _playPause() {
-//     if (isPlaying) {
-//       _audioPlayer.pause();
-//     } else {
-//       _audioPlayer.play(AssetSource(musicList[currentIndex].filePath));
-//     }
-//
-//     setState(() {
-//       isPlaying = !isPlaying;
-//     });
-//   }
-//
-//   void _stop() {
-//     _audioPlayer.stop();
-//     setState(() {
-//       isPlaying = false;
-//     });
-//   }
-//
-//   void _next() {
-//     if (currentIndex < musicList.length - 1) {
-//       currentIndex++;
-//       _loadMusic();
-//       _playPause();
-//     }
-//   }
-//
-//   void _previous() {
-//     if (currentIndex > 0) {
-//       currentIndex--;
-//       _loadMusic();
-//       _playPause();
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('音樂撥放器'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Container(
-//               color: Colors.lightBlueAccent,
-//               child:Image.asset('images/f1.jpg'),
-//               height: 500,
-//               width: 500,
-//             ),
-//             Text(
-//               musicList[currentIndex].title,
-//               style: TextStyle(fontSize: 20),
-//             ),
-//             SizedBox(height: 10),
-//             Text(
-//               musicList[currentIndex].artist,
-//               style: TextStyle(fontSize: 16),
-//             ),
-//             SizedBox(height: 20),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 IconButton(
-//                   icon: Icon(Icons.skip_previous),
-//                   onPressed: _previous,
-//                 ),
-//                 IconButton(
-//                   icon: isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
-//                   onPressed: _playPause,
-//                 ),
-//                 IconButton(
-//                   icon: Icon(Icons.stop),
-//                   onPressed: _stop,
-//                 ),
-//                 IconButton(
-//                   icon: Icon(Icons.skip_next),
-//                   onPressed: _next,
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 
 
@@ -257,6 +153,7 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   AudioPlayer _audioPlayer = AudioPlayer();
   bool isPlaying = false;
   int currentIndex = 0;
+
   @override
   void initState() {
     super.initState();
@@ -267,23 +164,26 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   void _loadMusic() async {
     await _audioPlayer.stop();
 
-    // Check if mp3Files is not empty and currentIndex is in a valid range
-    if (widget.mp3Files.isNotEmpty && currentIndex >= 0 && currentIndex < widget.mp3Files.length) {
-      await _audioPlayer.play(AssetSource(widget.mp3Files[currentIndex]));
+    if (widget.mp3Files.isNotEmpty && currentIndex >= 0 &&
+        currentIndex < widget.mp3Files.length) {
+      await _audioPlayer.play(AssetSource(languageMusicMap[widget.mp3Files]
+          ?.elementAt(currentIndex)
+          .filePath ?? ''));
     } else {
       print('Invalid mp3Files or currentIndex');
     }
   }
 
 
-
   void _playPause() {
     if (isPlaying) {
       _audioPlayer.pause();
     } else {
-      // 檢查 currentIndex 是否在有效範圍內
-      if (currentIndex >= 0 && currentIndex < widget.mp3Files.length) {
-        _audioPlayer.play(AssetSource(widget.mp3Files[currentIndex]));
+      if (currentIndex >= 0 &&
+          currentIndex < (languageMusicMap[widget.mp3Files]?.length ?? 0)) {
+        _audioPlayer.play(AssetSource(languageMusicMap[widget.mp3Files]
+            ?.elementAt(currentIndex)
+            .filePath ?? ''));
       } else {
         print('Invalid currentIndex: $currentIndex');
       }
@@ -293,31 +193,65 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
       isPlaying = !isPlaying;
     });
   }
-
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
+  void _next() {
+    if (currentIndex < (languageMusicMap[widget.mp3Files]?.length ?? 0) -1) {
+      currentIndex++;
+      _loadMusic();
+      _playPause();
+    }
   }
+
+  void _previous() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      _loadMusic();
+      _playPause();
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Music Player'),
+        title: Text('音樂撥放器'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+
+          children: [
+            Container(
+              color: Colors.lightBlueAccent,
+              child: Image.asset('images/f1.jpg'),
+              height: 500,
+              width: 500,
+            ),
             Text(
-              'Song Title: ${widget.mp3Files[currentIndex].split('/').last.replaceAll('.mp3', '')}',
+              'Song Title: ${languageMusicMap[widget.mp3Files]
+                  ?.elementAt(currentIndex)
+                  .filePath
+                  .split('/')
+                  .last
+                  .replaceAll('.mp3', '') ?? ''}',
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _playPause,
-              child: Text(isPlaying ? 'Pause' : 'Play'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.skip_previous),
+                  onPressed: _previous,
+                ),
+                IconButton(
+                  icon: isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+                  onPressed: _playPause,
+                ),
+                IconButton(
+                  icon: Icon(Icons.skip_next),
+                  onPressed: _next,
+                ),
+              ],
             ),
           ],
         ),
@@ -325,3 +259,4 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     );
   }
 }
+
